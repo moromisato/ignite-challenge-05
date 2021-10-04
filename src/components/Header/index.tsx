f2562437e3deb407e29743ad9787b05683f26fb5
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-04 07:47:18 -0300

    created header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import styles from './header.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <img src="/images/Logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
