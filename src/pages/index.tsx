41f89bdad26ec9d485ec638afc3bcb950fc2a78e
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-04 23:58:50 -0300

    added initial post structure and style added prismic query for querying posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
 import Header from '../components/Header';
-
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -31,24 +35,52 @@ export default function Home({ postsPagination }: HomeProps) {
   return (
     <>
       <Header />
-      <div>
-        {postsPagination.results.map(post => (
-          <div>{post.data.title}</div>
-        ))}
-      </div>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link key={post.uid} href="/">
+              <div>
+                <strong className={styles.title}>{post.data.title}</strong>
+                <div className={styles.subtitle}>{post.data.subtitle}</div>
+                <div className={styles.iconsContainer}>
+                  <div>
+                    <FiCalendar className={styles.icon} size={20} />
+                    {format(
+                      new Date(post.first_publication_date),
+                      'dd MMM yyyy',
+                      {
+                        locale: ptBR,
+                      }
+                    )}
+                  </div>
+                  <div>
+                    <FiUser className={styles.icon} size={20} />
+                    {post.data.author}
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  // const postsResponse = await prismic.query();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      pageSize: 100,
+    }
+  );
 
   return {
     props: {
       postsPagination: {
-        next_page: false,
-        results: [],
+        next_page: postsResponse.next_page,
+        results: postsResponse.results,
       },
     },
   };
