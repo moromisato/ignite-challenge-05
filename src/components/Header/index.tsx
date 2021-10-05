0646a7d2d08f440b226070badb55a1e5dbf66e36
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-05 07:45:03 -0300

    added missing link to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './header.module.scss';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -5,7 +6,9 @@ export default function Header() {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <img src="/images/Logo.svg" alt="logo" />
+        <Link href="/">
+          <img src="/images/Logo.svg" alt="logo" />
+        </Link>
       </div>
     </header>
   );
