978e7499a9e702863ad32aa8464c66981d1b61f7
Luis Moromisato <moromisato.luis@gmail.com>
2021-10-06 08:08:40 -0300

    added banner, title, and details

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Header from '../../components/Header';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -26,20 +30,74 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Post({ post }: PostProps) {
+  console.log(post);
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <img src={post.data.banner.url.url} alt={post.data.banner.url.alt} />
+        <article className={styles.post}>
+          <strong>{post.data.title}</strong>
+          <div className={styles.postDetails}>
+            <div>
+              <FiCalendar className={styles.icon} size={20} />
+              {format(new Date(post.first_publication_date), 'dd MMM yyyy', {
+                locale: ptBR,
+              })}
+            </div>
+            <div>
+              <FiUser className={styles.icon} size={20} />
+              {post.data.author}
+            </div>
+            <div>
+              <FiClock className={styles.icon} size={20} /> 4 min
+            </div>
+          </div>
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.data.content }}
+          />
+        </article>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
 
-//   // TODO
-// };
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  console.log(JSON.stringify(response));
+
+  const post: Post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
